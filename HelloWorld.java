503546251d7980ba659d7173ee955c225e092643
Common-Graduate <2968799534@qq.com>
2022-02-26 14:56:08 +0800

    稍作修改

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld
 {
 	public static void main(String[] args)
 	{
-		System.out.println("I " + "love " + 2 + 3 + "you!");
+		System.out.println("I " + "love " + 2 + 3 + " you!");
 		//I love 23 you!
 		System.out.println(2 + 3 + " I " + "love " + "you!");
 		//5 I love you!
