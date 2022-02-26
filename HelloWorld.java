c906653051034cde4b933e6d30ff32991cad34e7
Common-Graduate <2968799534@qq.com>
2022-02-26 14:33:47 +0800

    简单的比较一下print方法

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld
+{
+	public static void main(String[] args)
+	{
+		System.out.println("I " + "love " + 2 + 3 + "you!");
+		//I love 23 you!
+		System.out.println(2 + 3 + " I " + "love " + "you!");
+		//5 I love you!
+	}
+}
